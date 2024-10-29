d24ad81cc8a58a9b4deaec34a42c64a01f589eb4
Dyff1 <155970045+Dyff1@users.noreply.github.com>
2024-10-29 12:13:21 +0200

    v1
    php version comparison

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Rank Albums</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<table width="1500" height="1320" border="1" class="table1">
+  <tbody>
+    <tr>
+      <th height="100" colspan="1" scope="col"><?php echo"Rank Albums"?>&nbsp;
+      <form method="POST">
+         <input type="text" name="euro" size="3"><br>
+         <input type="submit" value="-">   
+      </th>
+    </tr>
+    <tr>
+      <th height="273" colspan="1" scope="col"><table width="1058" border="0" class="sisa-table">
+        <tbody>
+          <tr>
+            <td width="294" height="100" align="center" valign="middle"></td>
+            <!--<td width="294" align="center" valign="middle"></td>
+            <td width="448" align="center" valign="middle"></td> -->
+          </tr>
+        </tbody>
+      </table></th>
+    </tr>
+  </tbody>
+</table>
+<table width="1500" height="214" border="0" class="bottom">
+  <tbody>
+    <tr>
+      <td height="208"></td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
